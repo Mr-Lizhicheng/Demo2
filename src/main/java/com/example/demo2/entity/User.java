52c7cc4ab2e1bb040b8a30f6bc31799c045ab092
Mr-Lizhicheng <3254162498@qq.com>
2024-06-19 15:08:27 +0800

    添加User测试类

---
+++ src/main/java/com/example/demo2/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.demo2.entity;
+
+public class User {
+    private String name;
+    public String password;
+
+    public String loginToken;
+
+    public User() {
+
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginToken() {
+        return loginToken;
+    }
+
+    public void setLoginToken(String loginToken) {
+        this.loginToken = loginToken;
+    }
+}
